e866e3a19b0383481aa1492e12b411e985c9122a
Tatiana <tgurieva04@gmail.com>
2020-02-01 12:45:51 -0400

    выполняла задание 8-го раздела

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 "use strict";
 
 var gulp = require("gulp");
+var del = require("del");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 var webp = require("gulp-webp");
 var imagemin = require("gulp-imagemin");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
 var svgstore = require("gulp-svgstore");
-
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
@@ -23,8 +25,7 @@ gulp.task("css", function () {
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
 });
 
 gulp.task("images", function () {
@@ -48,20 +49,52 @@ gulp.task("sprite", function () {
      inlineSvg: true
    }))
    .pipe(rename("sprite.svg"))
-   .pipe(gulp.dest("source/img"));
+   .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("source"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+   base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+    server: "build/"
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+));
+
+gulp.watch("source/less/**/*.less", gulp.series("css"));
+gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+gulp.task("refresh", function (done) {
+server.reload();
+done();
+});
+gulp.task("start", gulp.series("build", "server"));
